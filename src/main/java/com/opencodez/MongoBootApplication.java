a999deb27adf90cafaed6be1b8dceeb358593dc5
CedricWillemsEHB <cedric.willems@student.ehb.be>
2020-08-09 21:03:27 +0200

    Starting project to DnD REST API

--- src/main/java/com/opencodez/MongoBootApplication.java
+++ src/main/java/com/opencodez/MongoBootApplication.java
@@ -25,7 +25,7 @@ public class MongoBootApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		deleteAll();
-		addSampleData();
+		//addSampleData();
 		listAll();
 		findFirst();
 		findByRegex();
